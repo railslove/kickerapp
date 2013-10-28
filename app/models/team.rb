d2cdb5d63987453badcf7b2f32fa1a7969f6306a
Stephan Pavlovic <stephan@railslove.com>
2013-10-28 17:26:05 +0100

    add single player mode
    Signed-off-by: Stephan Pavlovic <stephan@railslove.com>

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,7 @@ class Team < ActiveRecord::Base
   belongs_to :player2, class_name: "User"
 
   scope :for_user, lambda { |user1_id| where("(player1_id = #{user1_id} OR player2_id = #{user1_id})")}
+  scope :for_single_user, lambda { |user1_id| where("(player1_id = #{user1_id} AND player2_id IS NULL)")}
   scope :for_users, lambda { |user1_id, user2_id| where("(player1_id = #{user1_id} OR player2_id = #{user1_id}) AND (player1_id = #{user2_id} OR player2_id = #{user2_id})")}
 
   def matches
@@ -27,11 +28,19 @@ class Team < ActiveRecord::Base
   def self.find_or_create(user_ids)
     users = User.where(id: user_ids)
     team = nil
-    team = Team.for_users(user_ids.first, user_ids.last).first
+    if users.size > 1
+      team = Team.for_users(user_ids.first, user_ids.last).first
+    else
+      team = Team.for_single_user(user_ids.first).first
+    end
     if team.nil? && users.size == 2
       team = Team.new
       team.player1_id = user_ids.first
       team.player2_id = user_ids.last
+      team.save
+    elsif team.nil? && users.size == 1
+      team = Team.new
+      team.player1_id = user_ids.first
       team.save
     end
     team

8bb0a41a71e525bf9aa72fd21fccf4e3696062df
Maximilian Schulz <m.schulz@kulturfluss.de>
2014-04-28 16:13:18 +0200

    Add german translation and make it the default language

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module Kickerapp
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :de
   end
 end

8475906be1b7b352706889ca06765ee37276aa68
Paul Wittmann <paul@railslove.com>
2014-09-15 13:52:38 +0200

    ApplicationController: set locale from URL parameter (howto: http://guides.rubyonrails.org/i18n.html)

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe ApplicationController, type: :controller do
+
+  describe 'set_locale' do
+    let(:controller) { ApplicationController.new }
+    
+    context 'reset to default locale' do
+      before do
+        allow(controller).to receive(:params).and_return({})
+      end
+
+      specify do
+        I18n.locale = :es
+        controller.send(:set_locale)
+        expect(I18n.locale).to eql I18n.default_locale
+      end
+    end
+    
+    context 'change locale' do
+      before do
+        allow(controller).to receive(:params).and_return({ locale: 'en'})
+      end
+
+      specify do
+        expect{ controller.send(:set_locale) }.to change{ I18n.locale }.from(I18n.default_locale).to(:en)
+      end
+    end
+  end
+
+end

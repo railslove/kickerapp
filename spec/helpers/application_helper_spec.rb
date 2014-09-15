7257bb713d27ab81287463d29acf866e9b750c07
Paul Wittmann <paul@railslove.com>
2014-09-15 17:11:33 +0200

    ApplicationHelper: added other_locale.

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe ApplicationHelper, type: :helper do
+  
+  describe '#other_locale' do
+    specify 'for de' do
+      expect(helper.other_locale(:de)).to eql :en
+    end
+
+    specify 'for en' do
+      expect(helper.other_locale(:en)).to eql :de
+    end
+
+    context 'for any other locale' do
+      before { allow(I18n).to receive(:default_locale).and_return :zh }
+      specify do
+        expect(helper.other_locale(:es)).to eql :zh
+      end
+    end
+  end
+
+end

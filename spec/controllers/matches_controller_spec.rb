7848977c6a6880929647282a7642ea913426ba06
Stephan Pavlovic <stephan@railslove.com>
2014-09-22 16:46:31 +0200

    add a few specs for matches_controller#update

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -20,7 +20,35 @@ describe MatchesController, type: :controller do
   end
 
   describe '#update' do
-
+    let(:league) { FactoryGirl.create(:league) }
+    let(:user) { FactoryGirl.create(:user, league: league, quota: 1200) }
+    let(:user2) { FactoryGirl.create(:user, league: league, quota: 1200) }
+    let(:team) { FactoryGirl.create(:team, player1_id: user.id) }
+    let(:team2) { FactoryGirl.create(:team, player1_id: user2.id) }
+    let!(:match) { FactoryGirl.create(:match, score: '6:1', winner_team_id: team.id, loser_team_id: team2.id, difference: 10, league_id: league.id) }
+    it 'changes nothing if only result changes' do
+      allow(subject).to receive(:current_league).and_return(league)
+      patch :update, { id: match.id, league_id: league.id,  winner_score: '6', loser_score: '2', crawling: false }
+      expect(user.reload.quota).to eql(1208)
+      expect(user.reload.winning_streak).to eql(1)
+      expect(user2.reload.quota).to eql(1192)
+    end
+    it 'changes points if crawling changes' do
+      allow(subject).to receive(:current_league).and_return(league)
+      patch :update, { id: match.id, league_id: league.id,  winner_score: '6', loser_score: '2', crawling: true }
+      expect(user.reload.quota).to eql(1213)
+      expect(user.reload.winning_streak).to eql(1)
+      expect(user2.reload.quota).to eql(1187)
+    end
+    it 'swap teams if winner scored less scores then loser' do
+      allow(subject).to receive(:current_league).and_return(league)
+      patch :update, { id: match.id, league_id: league.id,  winner_score: '3', loser_score: '6', crawling: false }
+      expect(user.reload.quota).to eql(1192)
+      expect(user.reload.winning_streak).to eql(0)
+      expect(user2.reload.quota).to eql(1208)
+      expect(user2.reload.winning_streak).to eql(1)
+      expect(match.reload.score).to eql('6:3')
+    end
   end
 
 end

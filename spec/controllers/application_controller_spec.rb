69ad24390878700f237dfc0ee83fb65785d447d2
Salim Hbeiliny <salim@railslove.com>
2014-09-23 15:15:08 +0200

    more application_controller specs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -27,4 +27,67 @@ describe ApplicationController, type: :controller do
     end
   end
 
+  describe 'require_league' do
+    controller do
+      before_filter :require_league
+      def index
+        render text: 'welcome to your league'
+      end
+    end
+
+    context 'current league is set' do
+      before do
+        allow(controller).to receive(:current_league).and_return(123)
+      end
+      it 'should not redirect' do
+        get :index
+        expect(response.body).to eq('welcome to your league')
+      end
+    end
+
+    context 'current league is not set' do
+      before do
+        allow(controller).to receive(:current_league).and_return(nil)
+      end
+      it 'should redirect to root_path' do
+        get :index
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe 'current_league' do
+    let!(:league) { create :league, slug: 'the-league' }
+
+    context 'current league is set in session' do
+      before do
+        session[:league] = 'the-league'
+      end
+      it{ expect(controller.current_league).to eql league }
+    end
+
+    context 'current league is passed to params[:league_id]' do
+      before do
+        allow(controller).to receive(:params).and_return({ league_id: 'the-league' })
+      end
+      it{ expect(controller.current_league).to eql league }
+    end
+
+    context 'current league is passed to params[:id]' do
+      before do
+        allow(controller).to receive(:params).and_return({ id: 'the-league' })
+      end
+      it{ expect(controller.current_league).to eql league }
+    end
+
+    context 'current league is not set' do
+      it{ expect(controller.current_league).to be_nil }
+    end
+  end
+
+  describe 'clear_current_league' do
+    before{ session[:league] = 'the-league' }
+    it{ expect{ controller.send(:clear_current_league) }.to change{ session[:league] }.from('the-league').to(nil) }
+  end
+
 end

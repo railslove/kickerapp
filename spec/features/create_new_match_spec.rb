90875dee3cf536ef2f53598eaf4a184b97235c47
Salim Hbeiliny <salim@railslove.com>
2014-10-01 17:48:12 +0200

    add more scenarios for match entry

--- spec/features/create_new_match_spec.rb
+++ spec/features/create_new_match_spec.rb
@@ -7,50 +7,154 @@ feature 'create new match' do
   let!(:player3) { FactoryGirl.create(:user, name: 'Player 3', league: league) }
   let!(:player4) { FactoryGirl.create(:user, name: 'Player 4', league: league) }
 
-  scenario 'with 4 players' do
-    visit new_league_match_path('the-league')
+  context 'with 4 players' do
+    scenario 'valid entries' do
+      visit new_league_match_path('the-league')
 
-    select 'Player 1', from: 'team1_player1'
-    select 'Player 2', from: 'team1_player2'
-    select 'Player 3', from: 'team2_player1'
-    select 'Player 4', from: 'team2_player2'
+      select 'Player 1', from: 'team1_player1'
+      select 'Player 2', from: 'team1_player2'
+      select 'Player 3', from: 'team2_player1'
+      select 'Player 4', from: 'team2_player2'
 
-    fill_in 'set1_team1', with: '6'
-    fill_in 'set1_team2', with: '3'
-    fill_in 'set2_team1', with: '7'
-    fill_in 'set2_team2', with: '5'
+      fill_in 'set1_team1', with: '6'
+      fill_in 'set1_team2', with: '3'
+      fill_in 'set2_team1', with: '7'
+      fill_in 'set2_team2', with: '5'
 
-    click_button I18n.t('matches.form.enter')
+      click_button 'Eintragen'
 
-    expect(current_path).to eql league_path('the-league')
-    expect(page).to have_content I18n.t('matches.create.success')
-    expect(page).to have_content 'Player 1'
-    expect(page).to have_content 'Player 2'
-    expect(page).to have_content 'Player 3'
-    expect(page).to have_content 'Player 4'
-    expect(page).to have_content '6:3'
-    expect(page).to have_content '7:5'
+      expect(current_path).to eql league_path('the-league')
+      expect(page).to have_content 'Das Spiel wurde eingetragen!'
+      expect(page).to have_content 'Player 1'
+      expect(page).to have_content 'Player 2'
+      expect(page).to have_content 'Player 3'
+      expect(page).to have_content 'Player 4'
+      expect(page).to have_content '6:3'
+      expect(page).to have_content '7:5'
+    end
+    scenario 'invalid entries with same player in both teams' do
+      pending 'code fix required'
+      visit new_league_match_path('the-league')
+
+      select 'Player 1', from: 'team1_player1'
+      select 'Player 2', from: 'team1_player2'
+      select 'Player 1', from: 'team2_player1'
+      select 'Player 4', from: 'team2_player2'
+
+      fill_in 'set1_team1', with: '6'
+      fill_in 'set1_team2', with: '3'
+      fill_in 'set2_team1', with: '7'
+      fill_in 'set2_team2', with: '5'
+
+      click_button 'Eintragen'
+
+      expect(current_path).to eql new_league_match_path('the-league')
+      expect(page).to have_content 'Das Spiel konnte nicht gespeichert werden.'
+    end
+    scenario 'invalid entries' do
+      pending 'code fix required'
+      visit new_league_match_path('the-league')
+
+      select 'Player 1', from: 'team1_player1'
+      select 'Player 2', from: 'team1_player2'
+      select 'Player 1', from: 'team2_player1'
+      select 'Player 4', from: 'team2_player2'
+
+      fill_in 'set1_team1', with: '6'
+      fill_in 'set1_team2', with: '3'
+      fill_in 'set2_team1', with: '7'
+      fill_in 'set2_team2', with: '5'
+
+      click_button 'Eintragen'
+
+      expect(current_path).to eql new_league_match_path('the-league')
+      expect(page).to have_content 'Das Spiel konnte nicht gespeichert werden.'
+    end
   end
 
-  scenario 'with 2 players' do
-    visit new_league_match_path('the-league')
+  context 'with 2 players' do
+    scenario 'selecting from team1_player1 and team2_player1' do
+      visit new_league_match_path('the-league')
 
-    select 'Player 1', from: 'team1_player1'
-    select 'Player 3', from: 'team2_player1'
+      select 'Player 1', from: 'team1_player1'
+      select 'Player 3', from: 'team2_player1'
 
-    fill_in 'set1_team1', with: '6'
-    fill_in 'set1_team2', with: '3'
-    fill_in 'set2_team1', with: '7'
-    fill_in 'set2_team2', with: '5'
+      fill_in 'set1_team1', with: '6'
+      fill_in 'set1_team2', with: '3'
+      fill_in 'set2_team1', with: '7'
+      fill_in 'set2_team2', with: '5'
 
-    click_button I18n.t('matches.form.enter')
+      click_button 'Eintragen'
 
-    expect(current_path).to eql league_path('the-league')
-    expect(page).to have_content I18n.t('matches.create.success')
-    expect(page).to have_content 'Player 1'
-    expect(page).to have_content 'Player 3'
-    expect(page).to have_content '6:3'
-    expect(page).to have_content '7:5'
+      expect(current_path).to eql league_path('the-league')
+      expect(page).to have_content 'Das Spiel wurde eingetragen!'
+      expect(page).to have_content 'Player 1'
+      expect(page).to have_content 'Player 3'
+      expect(page).to have_content '6:3'
+      expect(page).to have_content '7:5'
+    end
+    scenario 'selecting from team1_player1 and team2_player2' do
+      visit new_league_match_path('the-league')
+
+      select 'Player 1', from: 'team1_player1'
+      select 'Player 3', from: 'team2_player2'
+
+      fill_in 'set1_team1', with: '6'
+      fill_in 'set1_team2', with: '3'
+      fill_in 'set2_team1', with: '7'
+      fill_in 'set2_team2', with: '5'
+
+      click_button 'Eintragen'
+
+      expect(current_path).to eql league_path('the-league')
+      expect(page).to have_content 'Das Spiel wurde eingetragen!'
+      expect(page).to have_content 'Player 1'
+      expect(page).to have_content 'Player 3'
+      expect(page).to have_content '6:3'
+      expect(page).to have_content '7:5'
+    end
+    scenario 'selecting from team1_player2 and team2_player1' do
+      pending 'code fix required'
+      visit new_league_match_path('the-league')
+
+      select 'Player 1', from: 'team1_player2'
+      select 'Player 3', from: 'team2_player1'
+
+      fill_in 'set1_team1', with: '6'
+      fill_in 'set1_team2', with: '3'
+      fill_in 'set2_team1', with: '7'
+      fill_in 'set2_team2', with: '5'
+
+      click_button 'Eintragen'
+
+      expect(current_path).to eql league_path('the-league')
+      expect(page).to have_content 'Das Spiel wurde eingetragen!'
+      expect(page).to have_content 'Player 1'
+      expect(page).to have_content 'Player 3'
+      expect(page).to have_content '6:3'
+      expect(page).to have_content '7:5'
+    end
+    scenario 'selecting from team1_player2 and team2_player2' do
+      pending 'code fix required'
+      visit new_league_match_path('the-league')
+
+      select 'Player 1', from: 'team1_player2'
+      select 'Player 3', from: 'team2_player2'
+
+      fill_in 'set1_team1', with: '6'
+      fill_in 'set1_team2', with: '3'
+      fill_in 'set2_team1', with: '7'
+      fill_in 'set2_team2', with: '5'
+
+      click_button 'Eintragen'
+
+      expect(current_path).to eql league_path('the-league')
+      expect(page).to have_content 'Das Spiel wurde eingetragen!'
+      expect(page).to have_content 'Player 1'
+      expect(page).to have_content 'Player 3'
+      expect(page).to have_content '6:3'
+      expect(page).to have_content '7:5'
+    end
   end
 
   scenario 'with crawling' do
@@ -68,16 +172,16 @@ feature 'create new match' do
 
     check 'crawling1'
 
-    click_button I18n.t('matches.form.enter')
+    click_button 'Eintragen'
 
     expect(current_path).to eql league_path('the-league')
-    expect(page).to have_content I18n.t('matches.create.success')
+    expect(page).to have_content 'Das Spiel wurde eingetragen!'
     expect(page).to have_content 'Player 1'
     expect(page).to have_content 'Player 2'
     expect(page).to have_content 'Player 3'
     expect(page).to have_content 'Player 4'
     expect(page).to have_content '6:3'
     expect(page).to have_content '7:5'
-    expect(page).to have_content I18n.t('karl.crawling')
+    expect(page).to have_content 'Hier musste jemand krabbeln! Das sollte die Welt erfahren.'
   end
 end

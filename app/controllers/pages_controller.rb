179b3fd3089fd300fc500987d9418cf56f890602
Stephan Pavlovic <stephan@railslove.com>
2014-10-29 12:26:10 +0100

    only use one line for matches per week and league

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,10 +20,11 @@ class PagesController < ApplicationController
 
   def kpis
     start_date = Date.parse('08.09.2014')
+    @active_leagues = League.where.not(name: 'Railslove').select{|l|l.matches.count > 10}
+
     @user_per_week = User.reorder(:created_at).where('created_at > ?',start_date).group_by{|u| u.created_at.to_date.cweek}.map{|w|[w.first, w.last.count]}
-    matches = Match.reorder(:date).where('date > ?',start_date)
+    matches = Match.reorder(:date).where('date > ?',start_date).where(league_id: @active_leagues.map(&:id))
     @matches_per_week = matches.group_by{|m| m.date.to_date.cweek}.map{|w|[w.first, w.last.count]}
-    @matches_per_week_per_league = matches.group_by{|m| m.league_id}.map{|l| [l.first, l.last.group_by{|m| m.date.to_date.cweek}.map{|w|[w.first, w.last.count]}]}
   end
 
 end

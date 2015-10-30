adef832ad4d3c3d640e2319210e641da03434f53
Maximilian Schulz <m.schulz@kulturfluss.de>
2015-10-30 21:29:54 +0100

    Load all of a league's teams and their ranking by a single sql command
    Instead of calculating scores in ruby, we can harness the power of SQL
    and do all of it in our database.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
   before_filter :require_league
 
   def index
-    @teams = current_league.teams.for_doubles.ranked.sort_by(&:value).reverse
+    @teams = current_league.team_ranking
   end
 
   def show

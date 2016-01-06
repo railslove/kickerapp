daeef264839e25b1ab05915dd2ed0ee5c78eb1f2
Stephan Pavlovic <stephan@railslove.com>
2016-01-06 17:44:44 +0100

    use datetime line chart for matches

--- app/widgets/games_week_widget.rb
+++ app/widgets/games_week_widget.rb
@@ -3,9 +3,12 @@ widget :games_week do
   type "line"
   data do
     {
-      items: Match.reorder(:date).where(date: (10.weeks.ago..Time.now)).group_by{|m| m.date.to_date.cweek}.map{|w|[x: w.first, y: w.last.count]}.flatten,
+      items: Match.reorder(:date).where(date: (10.weeks.ago..Time.now)).group_by{|m| m.date.to_date.cweek}.map{|w|[x: w.last.first.date.beginning_of_week.to_date, y: w.last.count]}.flatten,
       y_axis: [0,100,200,300,400,500,600],
-      incomplete_from: Date.today.cweek
+      x_axis: {
+        type: 'datetime'
+      },
+      incomplete_from: Date.today.beginning_of_week
     }
   end
 end

b9549866fe96c71e9022cb9b4211f67b5d3de2c5
Johannes Heck <yoyostile@gmail.com>
2016-04-19 12:20:24 +0200

    mino clean up

--- app/uploaders/league_header_image_uploader.rb
+++ app/uploaders/league_header_image_uploader.rb
@@ -1,22 +1,9 @@
 class LeagueHeaderImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-
-  # storage :file
-  storage :fog
-
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
 
   # process :scale => [800, 300]
   # process resize_to_fit: [800, 300]
@@ -36,11 +23,5 @@ class LeagueHeaderImageUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
-      region: 'eu-central-1',
+      region: ENV["AWS_REGION"],
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_ACCESS_KEY"]
       }

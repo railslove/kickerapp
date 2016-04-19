abfcdf389bdf6826204424f8a3ba18f99f2a52f3
NicoMartens <nico@railslove.com>
2016-04-19 17:03:51 +0200

    use image version and save in full size

--- app/uploaders/league_header_image_uploader.rb
+++ app/uploaders/league_header_image_uploader.rb
@@ -5,15 +5,15 @@ class LeagueHeaderImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [1920, 1920]
+  #process resize_to_fit: [1920, 1920]
 
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
 
-  # version :normal do
-  #   process resize_to_fit: [800,300]
-  # end
+  version :normal do
+     process resize_to_fit: [1920,1920]
+  end
 
   def extension_white_list
     %w(jpg jpeg gif png)

8c746d39245da74a50965d74133c8baaf85ae1a7
Stephan Pavlovic <stephan@railslove.com>
2017-06-30 11:35:30 +0200

    update kpis

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   http_basic_authenticate_with name: "kicker", password: (ENV['ADMIN_PASS'] || 'secret_password'), only: :kpis unless Rails.env.development?
 
   def landing
+
   end
 
   def pebble_settings
@@ -31,7 +32,7 @@ class PagesController < ApplicationController
   end
 
   def kpis
-    start_date = Date.parse('01.08.2015')
+    start_date = Date.parse('01.01.2017')
     @active_leagues = League.where.not(name: 'Railslove').select{|l|(l.matches.count > 10) && (l.matches.first.date > 14.days.ago)}
 
     @user_per_week = User.reorder(:created_at).where('created_at > ?',start_date).group_by{|u| u.created_at.to_date.cweek}.map{|w|[w.first, w.last.count]}

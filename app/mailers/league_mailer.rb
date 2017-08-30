fffe36605d41f947ec660762286bea5308b162a5
Stephan Pavlovic <stephan@railslove.com>
2017-08-31 00:06:36 +0200

    configure custom email templates

--- app/mailers/league_mailer.rb
+++ app/mailers/league_mailer.rb
@@ -3,8 +3,8 @@ class LeagueMailer < BaseMailer
   def welcome(league, locale)
     I18n.with_locale(locale) do
       mandrill_mail({
-        template: "kicker-app-registration-#{locale}",
-        subject: I18n.t('league_mailer.welcome.subject'),
+        template: "#{ENV['game_type']}-app-registration-#{locale}",
+        subject: I18n.t("league_mailer.welcome.#{game_type}.subject"),
         to: league.contact_email,
         bcc: 'kicker@railslove.com',
         vars: {

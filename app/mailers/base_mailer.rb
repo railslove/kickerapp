c8f1753b3591eecda00ab2dd34d0424f8b7553c4
Stephan Pavlovic <stephan@railslove.com>
2017-08-31 00:25:55 +0200

    more custom email stuff for pingpong vs. kicker

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,6 +1,6 @@
 class BaseMailer < MandrillMailer::TemplateMailer
   include Rails.application.routes.url_helpers
-  default from: 'kicker@railslove.com', from_name: 'Railslove Kicker App'
+  default from: 'kicker@railslove.com', from_name: "Railslove #{ENV[GAME_TYPE].capitalize} App"
   # these will be used as default arguments to the mandrill_mail call
   # IMPORTANT: Read more about this method and its parameters when setting defaults or creating a new mailer
   # https://github.com/renz45/mandrill_mailer/#creating-a-new-mailer

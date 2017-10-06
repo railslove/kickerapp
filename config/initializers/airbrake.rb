fc3b85c7707c267db43834821cb2af064705bb13
Lars Brillert <lars@railslove.com>
2017-10-06 16:38:09 +0200

    ditch airbrake

--- config/initializers/airbrake.rb
+++
@@ -1,4 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = '141bf82e8cb0e901a1af629ce3655142'
-  # config.development_environments = [] # enable this if you ever want to send exceptions to Airbrake in development mode
-end

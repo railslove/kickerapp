75bc1d326fdd920a3613718f92e7272f716d5774
Thomas Schrader <endverbraucher@slog.io>
2018-01-24 20:01:44 +0100

    use filtered params only if needed

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,21 +5,21 @@ class MatchesController < ApplicationController
   has_mobile_fu false
 
   def index
-    @date = match_params['date'] && Date.parse(match_params['date']) || Date.today
+    @date = params['date'] && Date.parse(params['date']) || Date.today
     @matches = current_league.matches.by_date(@date)
   end
 
   def new
     @match = Match.new
-    @team1 = Team.find_or_create(match_params[:team1].values.reject(&:blank?)) if match_params[:team1]
-    @team2 = Team.find_or_create(match_params[:team2].values.reject(&:blank?)) if match_params[:team2]
+    @team1 = Team.find_or_create(params[:team1].values.reject(&:blank?)) if params[:team1]
+    @team2 = Team.find_or_create(params[:team2].values.reject(&:blank?)) if params[:team2]
     @sets = []
     3.times do |i|
-      @sets[i] = match_params["set#{i+1}"] if match_params["set#{i+1}"].present?
+      @sets[i] = params["set#{i+1}"] if params["set#{i+1}"].present?
     end
     @crawlings = []
     3.times do |i|
-      @crawlings[i] = match_params["crawling#{i+1}"] if match_params["crawling#{i+1}"].present?
+      @crawlings[i] = params["crawling#{i+1}"] if params["crawling#{i+1}"].present?
     end
   end
 
@@ -27,7 +27,7 @@ class MatchesController < ApplicationController
     param = {}
     matches = create_matches_from_params
     if !matches.map(&:errors).map(&:empty?).reduce(:&)
-      redirect_to new_league_match_path(current_league, match_params), alert: "#{t('matches.create.failure')} #{matches.map(&:errors).inspect}"
+      redirect_to new_league_match_path(current_league, params), alert: "#{t('matches.create.failure')} #{matches.map(&:errors).inspect}"
     else
       matches.first.update_team_streaks
       tracker do |t|
@@ -49,13 +49,13 @@ class MatchesController < ApplicationController
   def update
     @match = Match.find(params[:id])
     @match.revert_points
-    if match_params[:winner_score].to_i < match_params[:loser_score].to_i
-      @match.score = @match.score_for_set(match_params[:loser_score], match_params[:winner_score])
+    if params[:winner_score].to_i < params[:loser_score].to_i
+      @match.score = @match.score_for_set(params[:loser_score], params[:winner_score])
       @match.swap_teams
     else
-      @match.score = @match.score_for_set(match_params[:winner_score], match_params[:loser_score])
+      @match.score = @match.score_for_set(params[:winner_score], params[:loser_score])
     end
-    @match.crawling = match_params[:crawling]
+    @match.crawling = params[:crawling]
     @match.calculate_user_quotas
     current_league.update_badges
     if @match.save
@@ -76,7 +76,7 @@ class MatchesController < ApplicationController
 
   def shuffle
     @match = Match.new
-    user_ids = match_params[:user_ids].select{|id| id.present?}
+    user_ids = params[:user_ids].select{|id| id.present?}
     if user_ids.size >= 4
       user_ids = user_ids.sample(4) if user_ids.size > 4 #Choose 4 players from array
       teams = Team.shuffle(user_ids)
@@ -108,9 +108,9 @@ class MatchesController < ApplicationController
         if set.first.present? && set.last.present? # If the set has been played
           match = Match.create_from_set({
             score: set,
-            crawling: match_params["crawling#{i+1}"].present?,
-            team1: match_params[:team1],
-            team2: match_params[:team2],
+            crawling: params["crawling#{i+1}"].present?,
+            team1: params[:team1],
+            team2: params[:team2],
             league_id: league.id.to_s
           })
           matches << match

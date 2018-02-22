62d7a93d001f6f57bd3345c77d62261a68a17da7
Stephan Pavlovic <stephan@railslove.com>
2018-02-22 09:01:13 +0100

    fix carrerwave order

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
   if Rails.env.production? && ENV["AWS_ACCESS_KEY"].present?
-    config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
       region: ENV["AWS_REGION"],
@@ -10,6 +9,8 @@ CarrierWave.configure do |config|
     config.fog_directory = ENV['AWS_BUCKET']
     config.fog_public = true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{(60 * 60 * 24 * 30 * 6)}" } # ~6 months
+    config.storage = :fog
+
   else
     config.storage = :file
   end

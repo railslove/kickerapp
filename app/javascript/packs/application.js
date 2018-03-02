452eb480cf98b40eb48b6f9b219087b58cfebecf
Thomas Schrader <endverbraucher@slog.io>
2018-03-02 11:42:36 +0100

    initialize i18n stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,15 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import I18n from "i18n-js"
 
-console.log('Hello World from Webpacker')
+I18n.defaultLocale = 'de'
+I18n.locale = document.documentElement.lang
+I18n.translations = {}
+I18n.translations.de = require('json-loader!yaml-loader!../../../config/locales/js.de.yml').de
+I18n.translations.en = require('json-loader!yaml-loader!../../../config/locales/js.en.yml').en
+
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true)
-var ReactRailsUJS = require("react_ujs")
+var componentRequireContext = require.context('components', true)
+var ReactRailsUJS = require('react_ujs')
 ReactRailsUJS.useContext(componentRequireContext)

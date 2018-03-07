f75590986f3e353fc502be072ab1fd30caba7d3d
Stephan Pavlovic <stephan@railslove.com>
2018-03-07 16:04:54 +0100

    add more fields to match type

--- app/graphql/types/match_type.rb
+++ app/graphql/types/match_type.rb
@@ -4,5 +4,7 @@ Types::MatchType = GraphQL::ObjectType.define do
   field :score, !types.String
   field :crawling, !types.Boolean
   field :difference, !types.Int
-  field :winner_team, Types::TeamType
+  field :winner_team, !Types::TeamType
+  field :looser_teaam, !Types::TeamType
+  field :date, !types.String
 end

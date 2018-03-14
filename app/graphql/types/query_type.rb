696c7e4fb2d478520e43b8a2184ab8d2963d1643
Stephan Pavlovic <stephan@railslove.com>
2018-03-14 22:21:41 +0100

    add limit option for match type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,7 +2,7 @@ Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
   field :leagues, !types[Types::LeagueType] do
-    argument :limit, types.Int, default_value: 20, prepare: -> (limit, ctx) { [limit, 30].min }
+    argument :limit, types.Int, default_value: 20, prepare: -> (limit, ctx) { [limit, 100].min }
     argument :league_slug, types.String, prepare: -> (league_slug, ctx) { league_slug }
     resolve -> (obj, args, ctx) {
       leagues = League.limit(args[:limit]).order(matches_count: :desc)

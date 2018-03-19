3c270bed5e330d59b392722816620303bffa1a41
Stephan Pavlovic <stephan@railslove.com>
2018-03-19 23:16:52 +0100

    add graphql mutation for shuffle

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -12,4 +12,13 @@ Types::MutationType = GraphQL::ObjectType.define do
       league.users.create(name: args[:name], email: args[:email], image: args[:image])
     }
   end
+  field :shuffle, !types[Types::TeamType] do
+    argument :player1_id, !types.Int, prepare: -> (player1_id, ctx) { player1_id }
+    argument :player2_id, !types.Int, prepare: -> (player2_id, ctx) { player2_id }
+    argument :player3_id, !types.Int, prepare: -> (player3_id, ctx) { player3_id }
+    argument :player4_id, !types.Int, prepare: -> (player4_id, ctx) { player4_id }
+    resolve -> (obj, args, ctx) {
+      Team.shuffle([args[:player1_id], args[:player2_id], args[:player3_id], args[:player4_id]])
+    }
+  end
 end

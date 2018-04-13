1a89c3c850f20228c44d94dd77e3c4f4e9baf1f7
Stephan Pavlovic <stephan@railslove.com>
2018-04-13 18:28:29 +0200

    add default image

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,11 @@ Types::UserType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :quota, !types.Int
   field :name, !types.String
-  field :image, !types.String
+  field :image, !types.String do
+    resolve ->(obj, args, ctx) {
+      obj.image.present? ? obj.image : 'https://kicker.cool/open-graph-logo.png'
+    }
+  end
   field :number_of_wins, !types.Int
   field :number_of_losses, !types.Int
   field :longest_winning_streak_games, types.Int
